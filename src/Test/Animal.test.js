eca6b1206a67c75d67b39cae3843aa464b3c5dbd
Collins <mussocollins@gmail.com>
2022-09-10 06:46:05 +0300

    Correct linters

--- src/Test/Animal.test.js
+++ src/Test/Animal.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux/ConfigureStore';
+import store from '../redux/configureStore';
 import Animal from '../components/Animals/Animal';
 
 test('renders correctly', () => {
--- src/Test/Animals.test.js
+++ src/Test/Animals.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux/ConfigureStore';
+import store from '../redux/configureStore';
 import Animals from '../components/Animals/Animals';
 
 test('renders correctly', () => {
