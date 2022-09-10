8488b48b43353ba8df3ce6463c2176f7b24f2236
Collins <mussocollins@gmail.com>
2022-09-10 06:27:21 +0300

    Add tests

---
+++ src/Test/Animals.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/ConfigureStore';
+import Animals from '../components/Animals/Animals';
+
+test('renders correctly', () => {
+  const tree = Renderer.create(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Animals />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
