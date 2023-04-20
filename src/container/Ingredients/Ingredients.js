b567d6c212eb7f30b574e4506497f8360cc65e4e
Collins <mussocollins@gmail.com>
2023-04-20 05:08:13 +0300

    Add imports to Ingredients component

--- src/container/Ingredients/Ingredients.js
+++ src/container/Ingredients/Ingredients.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 } from 'uuid';
+import { useNavigate, useOutlet } from 'react-router-dom';
+import {
+  clearMealList,
+  fetchIngredients,
+  updateIngredientsLoaded,
+  updateMealListLoaded
+} from '../../redux/actions/actionCreate';
+import { formatString } from '../Utils/Utils';
+import './Ingredients.scss';
+
 
 const Ingredients = () => {
   return (
-    <>
+    <main className="ingredients">
       
-    </>
+    </main>
   )
-}
+};
 
 export default Ingredients;
