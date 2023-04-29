cc10c8545dd834fd7287ab786f8d57466430b06e
Collins <mussocollins@gmail.com>
2023-04-29 11:40:25 +0300

    Add utils

--- src/container/Meal/Meal.js
+++ src/container/Meal/Meal.js
@@ -16,6 +16,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { getIngredientMaterials, isValidString } from "../utils/utils";
 import { fetchMealInfo } from '../../redux/actions/actionCreate';
 import './MealList.scss';
 
--- src/container/Utils/Utils.js
+++ src/container/Utils/Utils.js
@@ -4,3 +4,22 @@ export const formatString = (string) => {
 };
 
 export const isValidString = (string) => string !== null && /[A-Za-z\d]/g.test(string);
+
+export const getIngredientMaterials = (object) => {
+  // Convert object to entries..
+  let result = [];
+  const arr = Object.entries(object);
+
+  arr.forEach((entry) => {
+    if (entry[0].startsWith('strIngredient') && isValidString(entry[1])) {
+      let pair = new Array(2);
+      pair[0] = entry[1];
+      result.push(pair);
+    } else if (entry[0].startsWith('strMeasure') && isValidString(entry[1])) {
+      const position = parseInt(entry[0].replace('strMeasure', '')) - 1;
+      result[position][1] = entry[1];
+    }
+  });
+
+  return result;
+};
